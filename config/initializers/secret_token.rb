5fc34f502086e898c9766975048227e25fd46800
Henry Darmajaya <hdarmajaya@atni.com>
2013-11-12 11:24:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComverseRailsAdmin::Application.config.secret_token = 'eea3ce2809d9e3a42796d2bbfdc2c9a9a59267413831f9da6444cca3c08a4b65f3c2ee5a700a77339b0019d1d1b869899d1f192f6ee35863e3d72a84546c1873'
